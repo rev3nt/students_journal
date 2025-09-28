e7ab027044fe6582b65386e21a5e40acea3dc6e7
rev3nt <mikhial.merkulov.2005@mail.ru>
2025-09-28 13:40:00 +0300

    Interface  upgrade

--- main.py
+++ main.py
@@ -11,37 +11,44 @@ def calculate_average_for_all_students(students_list):
 
     result = round(summ_of_avg / len(students_list))
 
-    return result
+    print(f'Общий средний балл по всем студентам: {result}\n')
+
+def add_student(student_dict):
+    students.append(student_dict)
+
+    avg_students_grades.append(calculate_average(students[-1]['grades']))
+
+    calculate_average_for_all_students(students)
+
+def delete_student_with_least_avg(students_list):
+    students.pop(avg_students_grades.index((min(avg_students_grades))))
+
+    avg_students_grades.pop(avg_students_grades.index((min(avg_students_grades))))
+
+    calculate_average_for_all_students(students)
+
+def print_journal(students_list):
+    for i in range(len(students_list)):
+        print(f'Студент: {students_list[i]["name"]}')
+
+        print(f'Средний балл: {avg_students_grades[i]}')
+
+        print(f'Статус: {'Успешен' if avg_students_grades[i] > 75 else 'Отстающий'}\n')
 
 
 students = [
     {'name': 'John', 'grades': [80, 90, 78]},
     {'name': 'Michael', 'grades': [78, 75, 82]},
-    {'name': 'Jenny', 'grades': [91, 83, 77]},
     {'name': 'Ken', 'grades': [60, 84, 66]},
     {'name': 'Sadoveg', 'grades': [65, 65, 65]},
 ]
 
 avg_students_grades = [calculate_average(students[i]['grades']) for i in range(len(students))]
 
+print_journal(students)
 
-for i in range (len(students)):
-    print(f'Студент: {students[i]["name"]}')
+add_student({'name': 'Jenny', 'grades': [91, 83, 77]})
 
-    print(f'Средний балл: {avg_students_grades[i]}')
+delete_student_with_least_avg(students)
 
-    print(f'Статус: {'Успешен' if avg_students_grades[i] > 75 else 'Отстающий'}\n')
-
-print(f'Общий средний балл по всем студентам: {calculate_average_for_all_students(students)}')
-
-students.append({'name': 'Mihail', 'grades': [95, 94, 93]})
-
-avg_students_grades.append(calculate_average(students[-1]['grades']))
-
-print(f'Общий средний балл по всем студентам: {calculate_average_for_all_students(students)}')
-
-students.pop(avg_students_grades.index((min(avg_students_grades))))
-
-avg_students_grades.pop(avg_students_grades.index((min(avg_students_grades))))
-
-print(f'Общий средний балл по всем студентам: {calculate_average_for_all_students(students)}')
+print_journal(students)
